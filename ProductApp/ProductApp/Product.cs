dd4e39cb3d122f2541666e6551efffd52eb459f4
AlbertDzhamg <dzhamgar11@mail.ru>
2022-03-22 23:09:22 +0300

    release v1

---
+++ ProductApp/ProductApp/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        // навигационное свойство
+        public Company Manufacturer { get; set; }
+    }
+
+}
